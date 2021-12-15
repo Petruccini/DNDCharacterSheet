6d82ad21900b64b14ff531f8ed7a19a6785a1e6e
Petruccini <pedro.david@op.painani.mx>
2021-12-14 23:31:27 -0600

    Initial commit

---
+++ app/src/main/java/co/mudii/dndcharactersheet/MainActivity.kt
@@ -0,0 +1,25 @@
+package co.mudii.dndcharactersheet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import co.mudii.dndcharactersheet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonRollDice.setOnClickListener {
+            val result = (1..20).random()
+            Toast.makeText(this, "You rolled a: $result", Toast.LENGTH_LONG).show()
+
+            binding.textViewDiceResult.text = "Roll a D20!\nLast result: $result"
+        }
+
+    }
+}
